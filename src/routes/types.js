93462e4201c406b0469d31b33026b355ae15a90d
Leonardo Redin Pinto Figueiró <leonardoredin@outlook.com>
2021-01-14 19:51:39 -0300

    adicionando delete de tipo

--- src/routes/types.js
+++ src/routes/types.js
@@ -5,23 +5,39 @@ import { Types } from '../models'
 const typesRouter = Router()
 
 typesRouter.get('/all', async (req, res) => {
-  const types = await Types.find({})
+  try {
+    const types = await Types.find({})
 
-  res.json({
-    success: true,
-    types,
-  })
+    res.json({
+      success: true,
+      types,
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Erro interno do servidor',
+      error,
+    })
+  }
 })
 
 typesRouter.get('/:type', async (req, res) => {
   const { type } = req.params
 
-  const dbType = await Types.findOne({ name: type })
+  try {
+    const dbType = await Types.findOne({ name: type })
 
-  res.json({
-    success: true,
-    type: dbType,
-  })
+    res.json({
+      success: true,
+      type: dbType,
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Erro interno do servidor',
+      error,
+    })
+  }
 })
 
 typesRouter.post('/create', async (req, res) => {
@@ -89,6 +105,32 @@ typesRouter.post('/update', async (req, res) => {
     success: true,
     type,
   })
+})
+
+typesRouter.delete('/delete', async (req, res) => {
+  const { name } = req.body
+
+  if (!name) {
+    res.status(400).json({
+      success: false,
+      message: 'O nome do tipo é obrigatório',
+    })
+  }
+
+  try {
+    await Types.findOneAndDelete({ name })
+
+    res.json({
+      success: true,
+      message: 'Tipo deletado!',
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Erro interno do servidor',
+      error,
+    })
+  }
 })
 
 export default typesRouter
